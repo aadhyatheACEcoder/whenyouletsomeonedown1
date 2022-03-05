5a55a1f6e626426d169f16605031e97973a03de5
Aadhya J <96498686+aadhyatheACEcoder@users.noreply.github.com>
2022-03-05 11:57:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function rickrolls(){
+    document.getElementById("title").style.display = "none"
+    document.getElementById("message").style.display = "inline-block"
+    document.getElementById("rickroll").style.display = "inline-block"
+    document.getElementById("warn").style.display = "none"
+}
